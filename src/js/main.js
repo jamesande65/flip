2e5c9b1ed3f1c3978a45febd2dacd46c4d94c34c
nasyrkulov <james.ande65@gmail.com>
2019-11-06 09:40:26 +0200

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,23 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
     });
 
     mobileMenu();
